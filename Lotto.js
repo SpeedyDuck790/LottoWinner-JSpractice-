20b55cad7da1f98919fd25c56c65133ebbe3e729
james-hill <jamesleehill@outlook.com>
2023-07-18 00:30:35 +1000

    coin

--- Lotto.js
+++ Lotto.js
@@ -5,27 +5,42 @@ function myFunction() {
     document.getElementById("num").value = x;
 }   
 
-function validateForm() {
-    let x = document.forms["signin"]["FirstName"].value;
-    // let y = document.forms["signin"]["LastName"].value;
-    // let z = document.forms["signin"]["LuckNumber"].value;
-    // let a = document.forms["signin"]["password"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
+// function validateForm() {
+//     let x = document.forms["signin"]["FirstName"].value;
+//      let y = document.forms["signin"]["LastName"].value;
+//     let z = document.forms["signin"]["LuckNumber"].value;
+//     let a = document.forms["signin"]["password"].value;
+//     if (x == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }else if (y == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }else if (z == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }else if (a == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }
+
+// }
 
 var bank = "Participants: <br>----------------------------------<br>";
 var partnum = 0;
 function save() {
+    document.getElementById("c").className = ""
     var x = document.getElementById("signin");
+    if (x.elements[0].value == "") {
+        return false;
+    }
     var text = "submitted info: <br>";
     var i;
     for (i = 0; i < x.length ;i++) {
         text += x.elements[i].value + "<br>";
     }
-    document.getElementById("c").innerHTML = "Submitted!";
+    document.getElementById("c").className = "enter"
+    
     bank += text+"----------------------------------<br>";
     document.getElementById("bank").innerHTML = bank;
     partnum++;
@@ -33,5 +48,11 @@ function save() {
 document.getElementById("Greet").innerHTML ="Participants:<br>"+partnum;
 document.getElementById("chance").innerHTML ="Chance of winning:<br>"+(1/partnum)*100+"%";
 document.getElementById("signin").reset();
+
 }
+
+const myImage = new Image(100,100);
+myImage.src = "coin.png";
+document.getElementById("c").appendChild(myImage);
+
 
